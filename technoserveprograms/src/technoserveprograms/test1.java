6c84b70001161303004b4314cac0d8e12c3f41d2
Monish Mohanan <Monish Mohanan@192.168.1.12>
2022-02-01 20:11:15 +0530

    First commit

---
+++ technoserveprograms/src/technoserveprograms/test1.java
@@ -0,0 +1,12 @@
+package technoserveprograms;
+
+public class test1 {
+	public void h1(){
+		System.out.println("test1 h1");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
